e6c1972998bb371c7e535744df939cee5f304ec4
milad ramavandi <miladramavandi@milads-MacBook-Air.local>
2024-12-13 19:05:18 +0330

    7:05pm

--- server.js
+++ server.js
@@ -85,4 +85,4 @@ const server = http.createServer((req, res) => {
 
 
 
-server.listen(3000, () => console.log("server in running..."));
+server.listen(8000, () => console.log("server in running..."));
